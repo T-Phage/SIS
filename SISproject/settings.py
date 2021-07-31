14cbad809d22cb96078c53c3c2ddf2abc2af397c
T-Phage <samuelgyamfi824@gmail.com>
2021-07-31 18:55:10 +0000

    cotting

--- SISproject/settings.py
+++ SISproject/settings.py
@@ -17,19 +17,17 @@ from . secrets import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'd0ahn-ysp@k%vp3_3y11w1++)+n4#9h6qq!3b7kdn4gv9_*kl#'
-SECRET_KEY = SECRET_KEY1
+SECRET_KEY = 'd0ahn-ysp@k%vp3_3y11w1++)+n4#9h6qq!3b7kdn4gv9_*kl#'
+# SECRET_KEY = SECRET_KEY1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sinfosystem.herokuapp.com']
-
 
 # Application definition
 
