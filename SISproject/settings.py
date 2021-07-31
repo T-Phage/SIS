62e452e163d306f7222e887d846200edbc50d38c
T-Phage <samuelgyamfi824@gmail.com>
2021-07-31 19:02:15 +0000

    cotting

--- SISproject/settings.py
+++ SISproject/settings.py
@@ -75,6 +75,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SISproject.wsgi.application'
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
